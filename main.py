127ba161b0235c6a2b9659bd2f86f2943cca89ab
Aditya <adityasharma1946@gmail.com>
2025-05-20 22:54:25 +0530

    fix: cors issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config import Base, engine
 import tables.users as user_table
 import routes.users as user_routes
@@ -6,9 +7,12 @@ import routes.users as user_routes
 user_table.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_routes.router)
-
-# @app.get("/")
-# async def root():
-#     return "Hello , World"
